0193409b52b5b6898d9135a58631c21d4ab2a550
Abdullah AVCI <aponkral@users.noreply.github.com>
2017-11-09 10:39:59 +0300

    Add files via upload

---
+++ languages/en.php
@@ -0,0 +1,27 @@
+<?php
+
+$language = array(
+"language" => "English",
+"timezone" => "UTC",
+"site_name" => "Incoming SMS",
+"brand_name" => "Mutlucell",
+"refresh" => "Refresh",
+"general_title" => "What is Lorem Ipsum?",
+"general_description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan feugiat ante, facilisis tempus velit molestie luctus. Duis ut hendrerit magna. Quisque blandit porta nunc, sit amet bibendum sapien mollis.",
+"messages_query_time" => "Time to query messages",
+"time" => "Time",
+"sender" => "Sender",
+"message" => "Message",
+"error_code" => "Error Code",
+"error_description" => "Description",
+"error_code_20" => "20",
+"error_description_20" => "The xml that is posted is missing or incorrect.",
+"error_code_23" => "23",
+"error_description_23" => "Your subscriber number or password is incorrect.",
+"error_code_30" => "30",
+"error_description_30" => "Account Activation not provided.",
+"loading" => "Loading...",
+"forbidden" => "Forbidden"
+);
+
+?>
--- languages/tr.php
+++ languages/tr.php
@@ -1,1 +1,27 @@
+<?php
 
+$language = array(
+"language" => "Türkçe",
+"timezone" => "Europe/Istanbul",
+"site_name" => "Gelen SMS",
+"brand_name" => "Mutlucell",
+"refresh" => "Yenile",
+"general_title" => "Lorem Ipsum Nedir?",
+"general_description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan feugiat ante, facilisis tempus velit molestie luctus. Duis ut hendrerit magna. Quisque blandit porta nunc, sit amet bibendum sapien mollis.",
+"messages_query_time" => "Mesajların sorgulanma zamanı",
+"time" => "Zaman",
+"sender" => "Gönderen",
+"message" => "Mesaj",
+"error_code" => "Hata Kodu",
+"error_description" => "Açıklama",
+"error_code_20" => "20",
+"error_description_20" => "Post edilen xml eksik veya hatalı.",
+"error_code_23" => "23",
+"error_description_23" => "Abone no ya da parolanız hatalı.",
+"error_code_30" => "30",
+"error_description_30" => "Hesap Aktivasyonu sağlanmamış.",
+"loading" => "Yükleniyor...",
+"forbidden" => "Yasak"
+);
+
+?>
